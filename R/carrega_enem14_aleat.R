1f1d13900bc915fd5e4559867eb7a07380186649
rodrigues-am <andremr@if.usp.br>
2016-02-20 13:49:56 -0200

    Carrega dados enem14.

--- R/carrega_enem14_aleat.R
+++ R/carrega_enem14_aleat.R
@@ -10,21 +10,26 @@ setwd("~/dados/enem/2014/DADOS/") # lembre de substituir pasta onde está o arqu
 library(data.table)
 
 # Cria arquivo reduzido a partir de seleção aleatória
-system("perl -ne 'print if (rand() < .01)' MICRODADOS_ENEM_2014.csv > ENEM_2014.csv")
+system("perl -ne 'print if (rand() < .01)' MICRODADOS_ENEM_2014.csv > enem14.csv")
+
+enem14<-read.csv(file="enem14.csv")
 
 # Lê a primeira linha - apenas para pegar o nome das variávies (colunas)
-ehead<-read.csv(file="MICRODADOS_ENEM_2014.csv",nrows=1)
+ehead<-read.csv(file="MICRODADOS_ENEM_2014.csv", nrows=1)
 
 # Ajusta nome das variáveis
-colnames(ENEM_2014)<-colnames(ehead)
+colnames(enem14)<-colnames(ehead)
 
 # Transforma em data.table
-ENEM_2014<-as.data.table(ENEM_2014)
+enem14<-as.data.table(enem14)
 
 ## Verifica dados
 # Mostra a strutura do banco de dados
-str(ENEM_2014)
+str(enem14)
 
 # Cria sumário da variável <<idade>>
-summary(ENEM_2014$IDADE)
-boxplot(ENEM_2014$IDADE) 
+summary(enem14$IDADE)
+boxplot(enem14$IDADE) 
+
+# Deleta df desnecessário
+rm(ehead)
